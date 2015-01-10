1f575d2a3356c41ffeebf7a01cd8aa94754c53c6
ctxnop <ctxnop@nopping.fr>
2015-01-10 17:53:25 +0100

    Initial commit

---
+++ create-nuget.ps1
@@ -0,0 +1,8 @@
+New-Item -ItemType Directory -Force -Path repository
+cd repository
+Get-ChildItem "../" -Filter *.autopkg | `
+Foreach-Object{
+	Write-NuGetPackage ..\$_
+}
+Remove-Item *.symbols.*
+cd ..
